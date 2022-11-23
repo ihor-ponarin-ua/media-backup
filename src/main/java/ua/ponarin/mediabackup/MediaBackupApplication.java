8cbb46ab402702980ff74db63431161a731d33d1
Ihor Ponarin <ihor_ponarin@epam.com>
2022-11-23 13:25:38 +0200

    Initial commit

---
+++ src/main/java/ua/ponarin/mediabackup/MediaBackupApplication.java
@@ -0,0 +1,13 @@
+package ua.ponarin.mediabackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaBackupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/ponarin/mediabackup/MediaBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ponarin.mediabackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaBackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
